72b83196a88a3e0cf55ab4fe5a814cf3eac7eb8c
rafael augusto ramirez bolaño <rafar1129@gmail.com>
2025-06-19 19:49:25 -0500

    feat: add the message and new features

--- src/js/sistema_interactivo.js
+++ src/js/sistema_interactivo.js
@@ -3,15 +3,16 @@ function enviarFormulario(){
 
 const edad = document.getElementById('edad').value
 const nombre = document.getElementById('nombre').value
+
+let messaje 
 if (isNaN(edad)) {
-    console.error("Error: Por favor ,  ingresa una edad valida en numeros.");
+     messaje = "Error: Por favor ,  ingresa una edad valida en numeros.";
 } else if (edad >= 18) {
-    alert("Hola " + nombre + ", eres mayor de edad. ¡Preparate para grandes opórtunidades en el mundo de la programacion!");
+     messaje = "Hola " + nombre + ", eres mayor de edad. ¡Preparate para grandes opórtunidades en el mundo de la programacion!";
 
 }else {
-    alert("Hola " + nombre + ", eres menor de edad. ¡No te preocupes, la programacion es para todos! Sigue aprendiendo y creciendo.");
+    messaje = "Hola " + nombre + ", eres menor de edad. ¡No te preocupes, la programacion es para todos! Sigue aprendiendo y creciendo.";
 }
-
-
+    document.getElementById("resultado").innerHTML= messaje;
         
 } 
